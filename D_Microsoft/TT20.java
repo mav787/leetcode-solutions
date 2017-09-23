7f4fd14bcedf13441b5339e73894584bc52b897c
Haotian Sun <htsun1993@foxmail.com>
2017-09-23 16:49:54 -0400

    update

---
+++ D_Microsoft/TT20.java
@@ -0,0 +1,17 @@
+package D_Microsoft;
+import java.util.*;
+public class TT20 {
+	public boolean isValid(String s) {
+        if(s == null || s.length() % 2 != 0) return false;
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(c == '(') stack.push(')');
+            else if(c == '[') stack.push(']');
+            else if(c == '{') stack.push('}');
+            
+            else if(stack.isEmpty() || c != stack.pop()) return false;
+        }
+        return stack.isEmpty();
+    }
+}
