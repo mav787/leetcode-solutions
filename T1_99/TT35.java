7569cabc71c23cf4dff788a363fedb4d7877893f
mav787 <htsun1993@foxmail.com>
2021-09-18 12:53:55 -0700

    Update TT35.java

--- T1_99/TT35.java
+++ T1_99/TT35.java
@@ -2,42 +2,34 @@ package T1_99;
 
 public class TT35 {
 	public int searchInsert(int[] nums, int target) {
-        if(nums == null || nums.length == 0)
-            return 0;
+        if(nums == null || nums.length == 0){
+            return -1;
+        }
+        
         int left = 0, right = nums.length - 1;
         while(left + 1 < right){
             int mid = left + (right - left) / 2;
-            if(nums[mid] < target)
-                left = mid;
-            else if(nums[mid] == target)
-                return mid;
-            else
+            if(nums[mid] > target){
                 right = mid;
+            }
+            else if(nums[mid] == target){
+                return mid;
+            }
+            else{
+                left = mid;
+            }
         }
         
-        if(nums[right] == target)
-            return right;
-        if(nums[left] == target)
-            return left;
+        //return nums[left] == target ? left : right;
         
-        if(nums[right] < target)
+        if(nums[right] < target){
             return right + 1;
-        if(nums[left] < target)
-            return left + 1;
-        return left;
-        
-//        if(nums[right] == target)
-//            return right;
-//        else if(nums[left] == target)
-//            return left;
-//        
-//        else {
-//            if(nums[right] < target)
-//                return right + 1;
-//            else if(nums[left] < target && target < nums[right])
-//                return right;
-//            else
-//                return left;
-//        }
+        }
+        else if(nums[left] < target){
+            return right;
+        }
+        else{
+            return left;
+        }
     }
 }
